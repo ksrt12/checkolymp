08faa80fffa4a59c23942fcae42d6854e379adcd
ksrt12 <ksrt12group@gmail.com>
2020-06-27 11:21:38 +0300

    first commit

---
+++ checkAllOlymp_files/diploma.meta.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        CheckOlymps
+// @version     1.0
+// @date        2020-06-27
+// @author      kazakovstepan
+// @description Get all abiturient olymps
+// @homepage    https://vk.com/kazakovstepan
+// @icon        https://raw.githubusercontent.com/ksrt12/checkolymp/master/checkAllOlymp_files/icon.png
+// @updateURL   https://raw.githubusercontent.com/ksrt12/checkolymp/master/checkAllOlymp_files/diploma.meta.js
+// @downloadURL https://raw.githubusercontent.com/ksrt12/checkolymp/master/checkAllOlymp_files/diploma.user.js
+// @include     http://*
+// @include     https://*
+// @run-at      document-end
+// @grant       GM_listValues
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_info
+// @grant       GM_openInTab
+// @grant       GM_setClipboard
+// @grant       GM_registerMenuCommand
+// @grant       GM_unregisterMenuCommand
+// @grant       GM_notification
+// @grant       GM_download
+// @grant       GM.info
+// @grant       GM.listValues
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @grant       GM.deleteValue
+// @grant       GM.openInTab
+// @grant       GM.setClipboard
+// @grant       GM.xmlHttpRequest
+// @connect     isu.ifmo.ru
+// ==/UserScript==
