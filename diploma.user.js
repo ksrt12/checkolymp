285adb1947d6e4abdc9de91e566253a5644e4a59
ksrt12 <ksrt12group@gmail.com>
2020-07-09 01:09:26 +0300

    update to 3.2
    rewrite addcheck() function
    add addOlympCheck() function
    
    Signed-off-by: ksrt12 <ksrt12group@gmail.com>

--- diploma.user.js
+++ diploma.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Check All Olymps
-// @version     3.1
+// @version     3.2
 // @date        2020-07-09
 // @author      kazakovstepan
 // @namespace   ITMO University
@@ -15,30 +15,36 @@
 // ==/UserScript==
 
 function addcheck(str,ISUid){
-	var сheckolymp = document.createElement("button");
-	сheckolymp.id="OLYMP_CHECK";
-	сheckolymp.value=str;
-	сheckolymp.className="btn btn-labeled ";
-	сheckolymp.type="button";
-	сheckolymp.style="margin-right: 5px;"
 	var ISUELEM = document.getElementById(ISUid);
 	if (ISUELEM != null) {
+	var сheckolymp = document.createElement("button");
+		сheckolymp.id="OLYMP_CHECK";
+		сheckolymp.value=str;
+		сheckolymp.className="btn btn-labeled ";
+		сheckolymp.type="button";
+		сheckolymp.style="margin-right: 5px;"
 		ISUELEM.parentNode.insertBefore(сheckolymp, ISUELEM);
 		сheckolymp.insertAdjacentHTML('beforeend', '<span class="btn-label icon fa fa-refresh"></span>'+str);
-		сheckolymp.onclick=function(){addOLYMPlink()};
+		сheckolymp.onclick=function(){
+			if (ISUid == "PERS_UPDATE") {window.open(loadISU(),'_blank')}
+			else if (ISUid == "OLYMP_DELETE") {window.open(addOlympCheck(),'_blank')};
+		};
 	}
 }
 
 function loadISU(){
-	LN=document.getElementById('ST_LASTNAME').value;
-	FN=document.getElementById('ST_FIRSTNAME').value;
-	MN=document.getElementById('ST_MIDDLENAME').value;
-	BD=document.getElementById('ST_DOB').value.split('.');
+	var LN=document.getElementById('ST_LASTNAME').value;
+	var FN=document.getElementById('ST_FIRSTNAME').value;
+	var MN=document.getElementById('ST_MIDDLENAME').value;
+	var BD=document.getElementById('ST_DOB').value.split('.');
+	return 'https://ksrt12.github.io?LN='+LN+'&FN='+FN+'&MN='+MN+'&BDD='+BD[0]+'&BDM='+BD[1]+'&BDY='+BD[2]
 }
 
-function addOLYMPlink(){
-	loadISU();
-	window.open('https://ksrt12.github.io?LN='+LN+'&FN='+FN+'&MN='+MN+'&BDD='+BD[0]+'&BDM='+BD[1]+'&BDY='+BD[2],'_blank');
+function addOlympCheck(){
+	var olink = 'https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-'+
+		document.getElementById('OLYMP_YEAR').value+'/by-code/'+
+		document.getElementById('OLYMP_NUM').value.replace(/[. -]+/g, "")+'/white.pdf'
+    return olink;
 }
 
 function autophotocopy(){
@@ -54,5 +60,6 @@ function autophotocopy(){
 }
 
 addcheck("Проверить олимпиады","PERS_UPDATE");
+// addcheck("Проверить", "OLYMP_DELETE");
 
 autophotocopy();
