d621d13d37b1ba9628b64c14698cca2d822e4dc0
Kazakov Stepan <ksrt12group@gmail.com>
2020-08-11 00:52:42 +0300

    update to 5.5
    TEST

--- diploma.user.js
+++ diploma.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        Абитуриент 2.2
-// @version     5.2
-// @date        2020-08-08
+// @name        Абитуриент
+// @version     5.5
+// @date        2020-08-11
 // @author      kazakovstepan
 // @namespace   ITMO University
 // @description IT's MOre than the Система Абитуриент
@@ -18,19 +18,29 @@ function getID(someID) {
 	return document.getElementById(someID);
 }
 
+function NotifyErr(str) {
+	G2.notify(str, 'Ошибка', true);
+}
+
+function NotifyWarn(str) {
+	G2.notify(str, null, false, true);
+}
+
+var EGE_points = {}, OLYMPSbyName = {};
+
 // make buttons
 function addCheckButton(str,ISUid,func) {
 	var ISUELEM = getID(ISUid);
-	if (ISUELEM !== null) {
+	if ((ISUELEM !== null) && (getID("ButCheck") === null)) {
 	var CheckButton = document.createElement("button");
-		CheckButton.id = "OLYMP_CHECK";
+		CheckButton.id = "ButCheck";
 		CheckButton.value = str;
 		CheckButton.className = "btn btn-labeled ";
 		CheckButton.type = "button";
 		CheckButton.style = "margin-right: 5px;";
 		ISUELEM.parentNode.insertBefore(CheckButton, ISUELEM);
 		CheckButton.insertAdjacentHTML('beforeend', '<span class="btn-label icon fa fa-refresh"></span>'+str);
-		CheckButton.onclick = function() {window.open(func,'_blank');};
+		CheckButton.onclick = func;
 	}
 }
 
@@ -106,43 +116,239 @@ function autoEGE() {
 }
 
 function listenOLYMP() {
-	if ((getID('OLYMP_CHECK') === null) && (getID('OLYMP_DELETE') !== null) && (getONUM() !== "")) {
-		addCheckButton("Проверить", "OLYMP_DELETE", addOlympCheck());
+	if ((getID('ButCheck') === null) && (getID('OLYMP_DELETE') !== null) && (getONUM() !== "")) {
+		addCheckButton("Проверить", "OLYMP_DELETE", function () {window.open(addOlympCheck(),'_blank')});
 	}
+}
+
+function getSelectedText(elem) {
+	return elem.options[elem.options.selectedIndex].text;
 }
 
 function checkBVIwoAGREE() {
 	var bvi, agree;
 	var LK_AGREE = getID('LK_AGREE');
-	for (var i of document.querySelectorAll("#report_rating_rep > tbody > tr > td:nth-child(5)")) {
-		if (i.innerText === 'без вступительных испытаний') {
-			bvi = i.closest('tr').querySelector('td:nth-child(2)').innerText.substr(0,8);
-			agree = LK_AGREE.options[LK_AGREE.options.selectedIndex].text.substr(0,8);
+	for (var i of document.querySelectorAll("#report_rating_rep > tbody > tr")) {
+		if (i.querySelector('td:nth-child(5)').innerText === 'без вступительных испытаний') {
+			bvi = i.querySelector('td:nth-child(2)').innerText.substr(0,8);
+			agree = getSelectedText(LK_AGREE).substr(0,8);
 			if (agree !== bvi) {
-				G2.notify('БВИ без согласия!','Ошибка',true);
+				NotifyErr('БВИ без согласия!');
 			}
 		}
+	}
+}
+
+function getMinPoints(stream) {
+	var points, math, subj;
+	if (stream === '01.03.02') {
+		math = 76;
+	} else {
+		math = 62;
+	}
+	var minpoints = {'Математика': math, 'Русский язык': 60};
+	if ((stream === '12.03.03') ||
+		(stream === '12.05.01') ||
+		(stream === '16.03.01') ||
+		(stream === '16.03.03')) {
+			points = 60;
+			subj = 'Физика';
+	} else if ((stream === '18.03.02') || (stream === '19.03.01')) {
+		points = 60;
+		subj = 'Химия';
+	} else if (stream === '12.03.04') {
+		points = 60;
+		subj = 'Биология';
+	} else if (stream === '38.03.05') {
+		points = 60;
+		subj = 'Обществознание';
+	} else if ((stream === '27.03.05') || (stream === '45.03.04')) {
+		points = 60;
+		subj = 'Иностранный язык';
+	} else if (stream === '01.03.02') {
+		points = 75;
+		subj = 'Информатика';
+	} else {
+		points = 61;
+		subj = 'Информатика';
+	}
+	minpoints[subj] = points;
+	return minpoints;
+}
+
+function getVSEROS(stream) {
+	var vsosh = {'Математика': true};
+	var subj;
+	switch (stream) {
+		case '01.03.02':
+		case '09.03.01':
+		case '09.03.02':
+		case '09.03.03':
+		case '09.03.04':
+		case '10.03.01':
+		case '11.03.02':
+		case '11.03.03':
+		case '12.03.01':
+		case '13.03.02':
+		case '14.03.01':
+		case '15.03.04':
+		case '15.03.06':
+		case '24.03.02':
+		case '27.03.04':
+		case '44.03.04':
+			subj = ["Информатика"];
+			break;
+		case '12.03.04':
+		case '18.03.02':
+		case '19.03.01':
+			subj = ["Химия", "Биология"];
+			break;
+		case '12.03.02':
+		case '12.03.03':
+		case '12.03.05':
+		case '12.05.01':
+		case '16.03.01':
+		case '16.03.03':
+		case '23.03.03':
+			subj = ["Физика", "Астрономия"];
+			break;
+		case '45.03.04':
+			subj = ["Иностранный язык", "Информатика"];
+			break;
+		case '27.03.05':
+			subj = ["Иностранный язык", "Обществознание", "Право"];
+			break;
+		case '38.03.05':
+			subj = ["Экономика", "Обществознание", "Право"];
+			break;
+	}
+	for (var i of subj) {
+		vsosh[i] = true;
+	}
+	return vsosh;
+}
+
+function loadEGEpoints() {
+	for (var i of document.querySelectorAll("#report_baki_ege_rep > tbody > tr")) {
+		EGE_points[i.querySelector('td:nth-child(2)').innerText] = Number(i.querySelector('td:nth-child(4)').innerText);
+	}
+}
+
+function loadOLYMPS() {
+	for (var i of document.querySelectorAll("#report_olymp_rep > tbody > tr > td:nth-child(1)")) {
+		var a = i.innerText;
+		var olymp_subj = a.substring(a.indexOf(' (')+2, a.indexOf(', '));
+		var olymp_name = a.substring(0, a.indexOf(' ('));
+		OLYMPSbyName[olymp_name] = olymp_subj;
+	}
+}
+
+function checkSTREAM() {
+	var points;
+	var err_count = 0, warn_count = 0;
+	var minpoints = getMinPoints(curr_stream);
+	var curr_stream = getSelectedText(getID('APPL_PROG')).substr(0,8);
+	var curr_olymp = getSelectedText(getID('APPL_OLYMP'));
+	var isBVI = (getSelectedText(getID('APPL_USL')) === 'без вступительных испытаний');
+	var isOlymps = (getID('report_olymp_rep') !== null);
+	console.log(OLYMPSbyName);
+	if (isBVI) {
+		// БВИ
+		if (isOlymps) {
+			if (OLYMPSbyName[curr_olymp] === 'Русский язык') {
+				NotifyErr(curr_olymp + ' (Русский язык)\n Не даёт БВИ!');
+				err_count++;
+			}
+		} else {
+			NotifyErr('Нет олимпиад!');
+			err_count++;
+		}
+	} else {
+		// не БВИ
+		for (var subj in minpoints) {
+			points = EGE_points[subj];
+			console.log(subj + ' ' + minpoints[subj] + ':' + points);
+			if (points === undefined) {
+				NotifyErr('Нет ЕГЭ! (' + subj +')');
+				err_count++;
+			} else if (points < minpoints[subj]) {
+				NotifyErr(curr_stream + ': ' + subj + ': ' + points + '\nМинимальный балл: ' + minpoints[subj]);
+				err_count++;
+			}
+		}
+	}
+	if (isOlymps) {
+		if (curr_olymp === '...' ) {
+			if (isBVI) {
+				NotifyErr('БВИ без олимпиады!');
+				err_count++;
+			} else {
+				NotifyWarn('Обнаружены непроставленные олимпиады');
+				warn_count++;
+			}
+		} else {
+			var curr_subj = OLYMPSbyName[curr_olymp];
+			if (curr_olymp === 'Всероссийская олимпиада школьников') {
+				if (getVSEROS(curr_stream)[curr_subj]) {
+					if (isBVI) {
+						G2.notify(curr_stream + ': ВСЕРОС!');
+						err_count++;
+					} else {
+						NotifyErr('ВСЕРОС без БВИ');
+					}
+				} else {
+					NotifyErr('Олимпиада не подходит!');
+					warn_count++;
+				}
+			} else {
+				points = EGE_points[curr_subj];
+				if ((points < 75) || (points === undefined )) {
+					NotifyErr('Олимпиада не подтверждена! (' + curr_subj + ': ' + points + ')');
+					err_count++;
+				}
+			}
+		}
+	}
+	if ((err_count === 0) && (warn_count === 0)) {
+		G2.notify(curr_stream + ': OK!');
+		getID('APPL_UPDATE').click();
+	}
+}
+
+function checkAPPL() {
+	var HASH = document.location.hash;
+	if (HASH === '#olymp') {
+		document.addEventListener('click', listenOLYMP);
+	} else {
+		document.removeEventListener('click', listenOLYMP);
+	}
+	if (HASH === '#appl') {
+		console.log("opened appl");
+		loadEGEpoints();
+		loadOLYMPS();
+		var applf = document.querySelector("#appl_form > div.panel-body");
+		applf.addEventListener('change', addCheckButton("Проверить", "APPL_UPDATE", checkSTREAM));
+		//addCheckButton("Проверить", "APPL_UPDATE", checkSTREAM);
 	}
 }
 
 function main() {
 	var url = document.location.href;
 	if ((url.includes('ST_FORM')) || (url.includes('=2175:2:'))) {
-		addCheckButton("Проверить олимпиады", "PERS_UPDATE", addAllOlympsCheck());
+		addCheckButton("Проверить олимпиады", "PERS_UPDATE", function () {window.open(addAllOlympsCheck(),'_blank')});
 	} else if ((url.includes('APPLICATIONS')) || (url.includes('=2175:4:'))) {
 		autoEGE();
-		window.addEventListener('hashchange', function() {
-			if (document.location.hash === '#olymp') {
-				document.addEventListener('click', listenOLYMP);
-			} else {
-				document.removeEventListener('click', listenOLYMP);
-			}
-		});
+		checkAPPL();
+		window.addEventListener('hashchange', checkAPPL);
 	} else if ((url.includes('SU_OFFICE')) || (url.includes('=2175:5:'))) {
-		var LK_UPDATE = getID('LK_UPDATE');
+		var LK_UPD = getID('LK_UPDATE');
 		var DZCH = getID('LK_DELO_0');
-		if (LK_UPDATE !== null) {LK_UPDATE.onclick = checkBVIwoAGREE;}
-		if (DZCH !== null) {autophotocopy(DZCH);}
+		if (LK_UPD !== null) {
+			LK_UPD.onclick = checkBVIwoAGREE;
+		}
+		if (DZCH !== null) {
+			autophotocopy(DZCH);
+		}
 	}
 }
 
