7a4eee37e9e5c68e153eeedaa244df29a6101e11
Kazakov Stepan <ksrt12group@gmail.com>
2020-09-14 01:02:10 +0300

    add some checks
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- js/diploma.js
+++ js/diploma.js
@@ -1,140 +1,4 @@
-function SHA256(s) {
-	var chrsz = 8;
-	var hexcase = 0;
-
-	function safe_add(x, y) {
-		var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-		var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
-		return (msw << 16) | (lsw & 0xFFFF);
-	}
-
-	function S(X, n) {
-		return (X >>> n) | (X << (32 - n));
-	}
-
-	function R(X, n) {
-		return (X >>> n);
-	}
-
-	function Ch(x, y, z) {
-		return ((x & y) ^ ((~x) & z));
-	}
-
-	function Maj(x, y, z) {
-		return ((x & y) ^ (x & z) ^ (y & z));
-	}
-
-	function Sigma0256(x) {
-		return (S(x, 2) ^ S(x, 13) ^ S(x, 22));
-	}
-
-	function Sigma1256(x) {
-		return (S(x, 6) ^ S(x, 11) ^ S(x, 25));
-	}
-
-	function Gamma0256(x) {
-		return (S(x, 7) ^ S(x, 18) ^ R(x, 3));
-	}
-
-	function Gamma1256(x) {
-		return (S(x, 17) ^ S(x, 19) ^ R(x, 10));
-	}
-
-	function core_sha256(m, l) {
-		var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
-		var HASH = new Array(0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19);
-		var W = new Array(64);
-		var a, b, c, d, e, f, g, h, i, j;
-		var T1, T2;
-
-		m[l >> 5] |= 0x80 << (24 - l % 32);
-		m[((l + 64 >> 9) << 4) + 15] = l;
-
-		for (i = 0; i < m.length; i += 16) {
-			a = HASH[0];
-			b = HASH[1];
-			c = HASH[2];
-			d = HASH[3];
-			e = HASH[4];
-			f = HASH[5];
-			g = HASH[6];
-			h = HASH[7];
-
-			for (j = 0; j < 64; j++) {
-				if (j < 16) W[j] = m[j + i];
-				else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
-
-				T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
-				T2 = safe_add(Sigma0256(a), Maj(a, b, c));
-
-				h = g;
-				g = f;
-				f = e;
-				e = safe_add(d, T1);
-				d = c;
-				c = b;
-				b = a;
-				a = safe_add(T1, T2);
-			}
-
-			HASH[0] = safe_add(a, HASH[0]);
-			HASH[1] = safe_add(b, HASH[1]);
-			HASH[2] = safe_add(c, HASH[2]);
-			HASH[3] = safe_add(d, HASH[3]);
-			HASH[4] = safe_add(e, HASH[4]);
-			HASH[5] = safe_add(f, HASH[5]);
-			HASH[6] = safe_add(g, HASH[6]);
-			HASH[7] = safe_add(h, HASH[7]);
-		}
-		return HASH;
-	}
-
-	function str2binb(str) {
-		var bin = Array();
-		var mask = (1 << chrsz) - 1;
-		for (var i = 0; i < str.length * chrsz; i += chrsz) {
-			bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32);
-		}
-		return bin;
-	}
-
-	function Utf8Encode(string) {
-		string = string.replace(/\r\n/g, '\n');
-		var utftext = '';
-
-		for (var n = 0; n < string.length; n++) {
-
-			var c = string.charCodeAt(n);
-
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			} else if ((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			} else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-
-		}
-
-		return utftext;
-	}
-
-	function binb2hex(binarray) {
-		var hex_tab = hexcase ? '0123456789ABCDEF' : '0123456789abcdef';
-		var str = '';
-		for (var i = 0; i < binarray.length * 4; i++) {
-			str += hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) +
-				hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF);
-		}
-		return str;
-	}
-
-	s = Utf8Encode(s);
-	return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
-}
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('u 1e(s){t o=8;t p=0;u q(x,y){t a=(x&F)+(y&F);t b=(x>>16)+(y>>16)+(a>>16);v(b<<16)|(a&F)}u S(X,n){v(X>>>n)|(X<<(G-n))}u R(X,n){v(X>>>n)}u O(x,y,z){v((x&y)^((~x)&z))}u P(x,y,z){v((x&y)^(x&z)^(y&z))}u Q(x){v(S(x,2)^S(x,13)^S(x,22))}u T(x){v(S(x,6)^S(x,11)^S(x,25))}u U(x){v(S(x,7)^S(x,18)^R(x,3))}u V(x){v(S(x,17)^S(x,19)^R(x,10))}u Y(m,l){t K=H D(1f,1g,1h,1i,1j,1k,1l,1m,1n,1o,1p,1q,1r,1s,1t,1u,1v,1w,1x,1y,1z,1A,1B,1C,1D,1E,1F,1G,1H,1I,1J,1K,1L,1M,1N,1O,1P,1Q,1R,1S,1T,1U,1V,1W,1X,1Y,1Z,20,21,23,26,27,28,29,2a,2b,2c,2d,2e,2f,2g,2h,2i,2j);t k=H D(2k,2l,2m,2n,2o,2p,2q,2r);t W=H D(I);t a,b,c,d,e,f,g,h,i,j;t n,J;m[l>>5]|=2s<<(24-l%G);m[((l+I>>9)<<4)+15]=l;B(i=0;i<m.C;i+=16){a=k[0];b=k[1];c=k[2];d=k[3];e=k[4];f=k[5];g=k[6];h=k[7];B(j=0;j<I;j++){L(j<16)W[j]=m[j+i];M W[j]=q(q(q(V(W[j-2]),W[j-7]),U(W[j-15])),W[j-16]);n=q(q(q(q(h,T(e)),O(e,f,g)),K[j]),W[j]);J=q(Q(a),P(a,b,c));h=g;g=f;f=e;e=q(d,n);d=c;c=b;b=a;a=q(n,J)}k[0]=q(a,k[0]);k[1]=q(b,k[1]);k[2]=q(c,k[2]);k[3]=q(d,k[3]);k[4]=q(e,k[4]);k[5]=q(f,k[5]);k[6]=q(g,k[6]);k[7]=q(h,k[7])}v k}u Z(a){t b=D();t c=(1<<o)-1;B(t i=0;i<a.C*o;i+=o){b[i>>5]|=(a.14(i/o)&c)<<(24-i%G)}v b}u 1a(a){a=a.2t(/\\r\\n/g,\'\\n\');t b=\'\';B(t n=0;n<a.C;n++){t c=a.14(n);L(c<E){b+=w.A(c)}M L((c>2u)&&(c<2v)){b+=w.A((c>>6)|2w);b+=w.A((c&N)|E)}M{b+=w.A((c>>12)|2x);b+=w.A(((c>>6)&N)|E);b+=w.A((c&N)|E)}}v b}u 1b(a){t b=p?\'2y\':\'2z\';t c=\'\';B(t i=0;i<a.C*4;i++){c+=b.1c((a[i>>2]>>((3-i%4)*8+4))&1d)+b.1c((a[i>>2]>>((3-i%4)*8))&1d)}v c}s=1a(s);v 1b(Y(Z(s),s.C*o))}',62,160,'||||||||||||||||||||||||||safe_add|||var|function|return|String||||fromCharCode|for|length|Array|128|0xFFFF|32|new|64|T2||if|else|63|Ch|Maj|Sigma0256|||Sigma1256|Gamma0256|Gamma1256|||core_sha256|str2binb|||||charCodeAt||||||Utf8Encode|binb2hex|charAt|0xF|SHA256|0x428A2F98|0x71374491|0xB5C0FBCF|0xE9B5DBA5|0x3956C25B|0x59F111F1|0x923F82A4|0xAB1C5ED5|0xD807AA98|0x12835B01|0x243185BE|0x550C7DC3|0x72BE5D74|0x80DEB1FE|0x9BDC06A7|0xC19BF174|0xE49B69C1|0xEFBE4786|0xFC19DC6|0x240CA1CC|0x2DE92C6F|0x4A7484AA|0x5CB0A9DC|0x76F988DA|0x983E5152|0xA831C66D|0xB00327C8|0xBF597FC7|0xC6E00BF3|0xD5A79147|0x6CA6351|0x14292967|0x27B70A85|0x2E1B2138|0x4D2C6DFC|0x53380D13|0x650A7354|0x766A0ABB|0x81C2C92E|0x92722C85|0xA2BFE8A1|0xA81A664B|0xC24B8B70|0xC76C51A3|0xD192E819|0xD6990624|0xF40E3585|0x106AA070|0x19A4C116||0x1E376C08|||0x2748774C|0x34B0BCB5|0x391C0CB3|0x4ED8AA4A|0x5B9CCA4F|0x682E6FF3|0x748F82EE|0x78A5636F|0x84C87814|0x8CC70208|0x90BEFFFA|0xA4506CEB|0xBEF9A3F7|0xC67178F2|0x6A09E667|0xBB67AE85|0x3C6EF372|0xA54FF53A|0x510E527F|0x9B05688C|0x1F83D9AB|0x5BE0CD19|0x80|replace|127|2048|192|224|0123456789ABCDEF|0123456789abcdef'.split('|'),0,{}))
 
 const RSROLYMP = 'https://diploma.rsr-olymp.ru/files/rsosh-diplomas-static/compiled-storage-',
 	WLS = window.location.search;
@@ -228,7 +92,7 @@ function getSubTitles(olympname, grad) {
 	if ((grad === 10) || (grad === 11)) {
 		status = checkBVI('01.03.02', t4, t1, t2, t3);
 	} else if ((grad === 9) || (grad === 8) || (grad === 7)) {
-		status = (t4.toLowerCase() === 'русский язык') ? wtf : ia + checkConf(t4.toLowerCase(), 60);
+		status = set_ia_status(t4)
 	} else {
 		status = wtf;
 	}
@@ -250,6 +114,7 @@ function update_diplomas(olympYear) {
 			d.form,
 			doa[4],
 		], doa[4]));
+		check_ege_is_empty(doa[3]);
 	}
 	target.appendChild(table);
 }
@@ -360,35 +225,94 @@ function update_status(stream) {
 			tbody.rows[i].cells[2].innerText);
 			tbody.rows[i].cells[6].innerHTML = new_status;
 			bvi_color(tbody.rows[i], new_status);
+			check_ege_is_empty(tbody.rows[i].cells[3].innerText);
 		}
 	}
 }
 
-function do_search(){
-	let old_table = document.getElementById('table');
-	old_table ? old_table.remove() : true;
+function set_ia_status(subj_in) {
+	let subj = subj_in.toLowerCase();
+	return (subj === 'русский язык') ? wtf : ia + checkConf(subj, 60);
+}
+
+function update_ia_status() {
+	for (let i = 0; i < tbody.rows.length; i++) {
+		if ((tbody.rows[i].cells[5].innerText === "9") || 
+			(tbody.rows[i].cells[5].innerText === "8") ||
+			(tbody.rows[i].cells[5].innerText === "7")) {
+			tbody.rows[i].cells[6].innerHTML = set_ia_status(tbody.rows[i].cells[3].innerText);
+			check_ege_is_empty(tbody.rows[i].cells[3].innerText);
+		}
+	}
+}
+
+function rename_button(to_upd) {
+	document.querySelector("#check_button > button").innerHTML = to_upd ? "Обновить" : "Проверить";
+}
+
+function check_ege_is_empty(conf_subj_in) {
+	if (WLS === "") {
+		let subj = SUBJ_EGE[conf_subj_in.toLowerCase()];
+		is_empty_color(document.querySelector('[for='+SUBJ_ID[subj]+']'), (EGE[subj] === 0));
+	}
+}
+function is_empty_color(elem, is_empty) {
+	elem.style = (is_empty) ? "background-color: pink;" : ""
+}
+
+function do_search() {
+	let is_table = document.getElementById('table');
 	EGE = {};
-	for (let i of document.querySelectorAll("#search_form > p > input")) {
-		params[i.id] = i.value.trim().toLowerCase().replace(/(([- ]|^)[^ ])/g, function(s) {
-			return s.toUpperCase();
-		});
-	}
 	for (let j of document.querySelectorAll(".ege > form > p > input")) {
-		EGE[document.querySelector('[for='+j.id+']').innerText.toLowerCase()] = Number(j.value);
+		let points = Number(j.value);
+		points = (points < 0) ? 0 : (points > 100) ? 100 : points;
+		j.value = points;
+		EGE[document.querySelector('[for='+j.id+']').innerText.toLowerCase()] = points;
 	}
-	params.NAME = (params.LN+' '+params.FN+' '+params.MN).replace(/\s+/g, ' ');
-	
-	new_table = make_table();
-	sort_table(new_table);
+	if (is_table) {
+		update_status('01.03.02');
+		update_ia_status();
+	} else {
+		for (let i of document.querySelectorAll("#search_form > p > input")) {
+			params[i.id] = i.value.trim().toLowerCase().replace(/(([- ]|^)[^ ])/g, function(s) {
+				return s.toUpperCase();
+			});
+		}
+		params.NAME = (params.LN+' '+params.FN+' '+params.MN).replace(/\s+/g, ' ');
+
+		if (params.LN && params.FN) {
+			let new_table = make_table();
+			sort_table(new_table);
+		} else {
+			console.log("empty")
+		}
+	}
+}
+
+function is_exist() {
+	return document.getElementById('table');
+}
+
+function check_data() {
+	let is_table = document.getElementById('table');
+	if (is_table) {
+		rename_button();
+		is_table.remove();
+	} else {
+		let SB = document.querySelector("#check_button > button");
+		let LN = document.querySelector("#LN").value.length;
+		let FN = document.querySelector("#FN").value.length;
+		SB.disabled = (LN && FN) ? false : true;
+	}
 }
 
 if (WLS !== "") {
 	window.addEventListener("DOMContentLoaded", function() {
 		document.getElementById('indata_id').remove();
 		document.getElementById('check_button').remove();
+		load_params();
+		new_table = make_table();
+		checktable(new_table);
 	});
-	load_params();
-	new_table = make_table();
-	checktable(new_table);
-}
+};
 
